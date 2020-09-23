d4b4fd20977c1b682fab716a3238e50b63d3efc2
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-09-23 23:31:41 +0200

    Better plot_faster_dyn.py

--- plot_faster_dyn.py
+++ plot_faster_dyn.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-from util import complete_df_1loc_1age
+from util import complete_df_1loc_1age, population
 
 from load_data import load_dataframe_dep, DEPARTMENTS
 
@@ -25,10 +25,12 @@ def plot_faster_dyn(
     if axes is None:
         fig, ax = plt.subplots()
         fig1, ax1 = plt.subplots()
+        fig2, ax2 = plt.subplots()
     else:
-        ax, ax1 = axes
+        ax, ax1, ax2 = axes
         fig = ax.figure
         fig1 = ax1.figure
+        fig2 = ax2.figure
 
     styles = ["-", "--", ":", "-."]
     for dep in DEPARTMENTS.keys():
@@ -60,16 +62,20 @@ def plot_faster_dyn(
         ):
 
             label = f"{dep:2} - {DEPARTMENTS[dep]}"
-            tmp.plot(y="ratio_c", ax=ax, label=label, style=style)
-            tmp.plot(y="incidence", ax=ax1, label=label, style=style)
+            tmp.plot(y="incidence", ax=ax, label=label, style=style)
+            tmp.plot(y="ratio_c", ax=ax1, label=label, style=style)
+            tmp["Tc_scaled"] = 100000 / population[dep] * tmp["Tc"]
+            tmp.plot(y="Tc_scaled", ax=ax2, label=label, style=style)
 
-    ax.set_title("Taux de positivité sur 7 jours")
-    ax1.set_title("Incidence (cas positifs sur 7 jours par 100000 hab.)")
 
-    ax1.axhline(50, color="k")
+    ax.set_title("Incidence (cas positifs sur 7 jours par 100000 hab.)")
+    ax1.set_title("Taux de positivité sur 7 jours")
+    ax2.set_title("Nombre de tests 7 derniers jours / 100000 hab.")
+
+    ax.axhline(50, color="k")
 
     date_bad_data = tmp.index[-3]
-    for _ in [ax, ax1]:
+    for _ in [ax, ax1, ax2]:
         _.axvline(date_bad_data, color="k", linestyle=":")
         _.legend(loc="upper left", prop={"size": 8})
 
@@ -80,5 +86,5 @@ def plot_faster_dyn(
 if __name__ == "__main__":
 
     plot_faster_dyn(verbose=True, min_incidence=120)
-    plot_faster_dyn(verbose=True, min_incidence=80, max_incidence=120)
+    plot_faster_dyn(verbose=False, min_incidence=80, max_incidence=120)
     plt.show()
